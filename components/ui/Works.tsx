47cc612b309137c3fbadeeb77300a7b018b5098c
unknown <wankenobi29@gmail.com>
2024-10-28 19:59:18 +0300

    Solutions page updated

--- components/ui/Works.tsx
+++ components/ui/Works.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { Button } from "./button";
 
 export default function Works() {
+  const imageStyle = {
+    borderTopRightRadius: "8%",
+    borderBottomLeftRadius: "8%",
+    border: "1px solid",
+  };
+
   const works = [
     {
       service: "WEB DESIGN",
@@ -10,10 +17,16 @@ export default function Works() {
         <Image
           src="/static/web-dev.png"
           alt="Web design"
-          width="400"
-          height="700"
+          width="470"
+          height="550"
+          style={imageStyle}
         />
       ),
+      subservices: [
+        "E-commerce websites",
+        "website redesign",
+        "website development,",
+      ],
     },
     {
       service: "GRAPHIC DESIGN",
@@ -25,8 +38,10 @@ export default function Works() {
           alt="Graphic Design"
           width="400"
           height="700"
+          style={imageStyle}
         />
       ),
+      subservices: ["LOGO Design", "Posters and Brochures"],
     },
     {
       service: "BRANDING",
@@ -38,8 +53,10 @@ export default function Works() {
           alt="Branding"
           width="400"
           height="700"
+          style={imageStyle}
         />
       ),
+      subservices: ["Brand Identity"],
     },
     {
       service: "APP DEVELOPMENT",
@@ -51,33 +68,79 @@ export default function Works() {
           alt="App Development"
           width="400"
           height="700"
+          style={imageStyle}
         />
       ),
+      subservices: [
+        "Android app development",
+        "IOS app development",
+        "Hybrid app development",
+        "Web app development",
+        "Software development",
+      ],
     },
     {
       service: "IT CONSULTING",
       description:
         "We offer IT consulting services designed to help businesses leverage technology for growth and efficiency. We focus on understanding our clients' unique needs and challenges, delivering tailored solutions that align with their goals. From optimizing existing systems to implementing new technologies, we work closely with clients to streamline processes, enhance cybersecurity, and ensure scalability. Our team stays current on industry trends and best practices, providing strategic insights that drive innovation and resilience in an ever-evolving tech landscape. With a commitment to reliability and partnership, we empower businesses to adapt, grow, and succeed with technology that works for them.",
-      img: "",
+      img: (
+        <Image
+          src="/static/Consultation.png"
+          alt="IT Consultation Services"
+          width="400"
+          height="700"
+          style={imageStyle}
+        />
+      ),
+      subservices: ["Consultational Services", "Training and development"],
     },
     {
       service: "DIGITAL MARKETING",
       description:
         "We provide digital marketing services that drive brand growth and connect businesses with their audience effectively. We develop tailored strategies that combine data-driven insights with creative content, ensuring each campaign reflects our clients' unique goals and brand voice. From social media management and SEO to targeted ads and content creation, we use a multi-channel approach to maximize reach and engagement. By working closely with clients, we adapt our strategies to their evolving needs, delivering results that increase visibility, attract leads, and foster customer loyalty. Through our expertise in digital marketing, we help brands stand out, grow, and achieve lasting success online.",
-      img: "",
+      img: (
+        <Image
+          src="/static/digital-marketing.webp"
+          alt="Digital Marketing"
+          width="400"
+          height="700"
+          style={imageStyle}
+        />
+      ),
+      subservices: [
+        "Social media management",
+        "Search engine optimization (SEO)",
+        "Email marketing",
+        "Paid advertising",
+        "Content marketing",
+        "Analytics and reporting",
+        "Digital analytics and reporting",
+        "Email marketing automation",
+        "Website analytics and reporting",
+      ],
     },
     {
       service: "SUPPORT AND MAINTENANCE",
       description:
         "We offer support and maintenance services to ensure that our clients’ digital platforms run smoothly and efficiently. We understand that consistent performance and quick resolution of issues are essential for a successful online presence, so we provide proactive monitoring, timely updates, and responsive troubleshooting. Our team works closely with clients to understand their specific needs, offering customized support plans that include everything from software updates to performance optimizations. By staying ahead of potential issues and maintaining open communication, we ensure our clients can focus on their business goals with confidence. With our commitment to reliability and client satisfaction, we keep digital platforms secure, up-to-date, and ready to perform at their best.",
-      img: "",
+      img: (
+        <Image
+          src="/static/IT-Maintenance-Support.avif"
+          alt="Support and Maintenance"
+          width="500"
+          height="700"
+          style={imageStyle}
+        />
+      ),
+      subservices: ["Troubleshooting"],
     },
-    {
+    /* {
       service: "SEARCH ENGINE OPTIMIZATION (SEO)",
       description:
         "We specialize in Search Engine Optimization (SEO) to help clients enhance their online visibility and attract quality traffic. Our SEO approach combines in-depth keyword research, on-page optimization, content strategy, and technical improvements tailored to each client’s unique goals and industry. We work closely with clients to understand their audience and business objectives, implementing strategies that improve rankings and drive organic growth. From optimizing website structure to creating engaging, keyword-rich content, we ensure every element aligns with search engine best practices. With a commitment to measurable results and continuous improvement, we help brands increase their search presence, attract leads, and achieve long-term success online.",
       img: "",
     },
+    */
   ];
 
   return (
@@ -87,13 +150,25 @@ export default function Works() {
           <div key={index} className="col-lg-4 col-md-6  w-screen">
             <div className="work-item flex flex-col items-center justify-center mt-16 mb-12">
               <div className="work-info flex-col  placecenter ">
-                <h3 className="text-8xl placecenter mb-9 text-center">
+                <h3 className="text-9xl placecenter mb-9 text-center w-2/3">
                   {work.service}
                 </h3>
-                <p className="text-center text-xl w-3/5">{work.description}</p>
-              </div>
-              <div className="work-img">
-                <div>{work.img} </div>
+                <div className="m-services flex flex-row w-screen">
+                  <div className=" flex flex-col h-auto w-3/5 ">
+                    <p className="text-center  text-xl mx-12">
+                      {work.description}
+                    </p>
+                    <section className=" placecenter flex gap-2 flex-wrap relative my-9">
+                      {work.subservices &&
+                        work.subservices.map((subservice, index) => (
+                          <Button key={index}>{subservice}</Button>
+                        ))}
+                    </section>
+                  </div>
+                  <div className="flex w-2/5 mb-4">
+                    <div>{work.img} </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- components/ui/solutionslanding.tsx
+++ components/ui/solutionslanding.tsx
@@ -1,6 +1,6 @@
 export default function SolutionsLanding() {
   return (
-    <div className="landingly w-screen h-screen flex justify-between">
+    <div className="landingly w-screen min-h-screen flex justify-between">
       <div className="uppertext absolute flex-col items-center justify-center w-2/5">
         <h2 className="text-8xl">THE FUTURE</h2>
         <h2 className="text-8xl">HAPPENING TODAY</h2>
