1243987bd14628c3e0ec963ee59b7912a58a0c42
unknown <wankenobi29@gmail.com>
2024-10-31 19:21:25 +0300

    responsive orby section

--- components/ui/explainer.tsx
+++ components/ui/explainer.tsx
@@ -8,8 +8,8 @@ interface OrbyProps {
 
 export default function Orby({ children }: OrbyProps) {
   return (
-    <div className="relative flex-col space-y-12 box-border h-screen w-screen">
-      <p className="sm:text-4xl text-center  lg:pt-24 lg:px-20 lg:text-6xl font-medium lg:leading-normal w-screen alternate-heading">
+    <div className="relative flex-col space-y-12 box-border h-auto w-screen landingly-mobile md:h-screen">
+      <p className="sm:text-4xl pt-0 mt-0 text-center  lg:pt-24 lg:px-20 lg:text-6xl font-medium lg:leading-normal w-screen alternate-heading">
         {" "}
         {children}
       </p>
--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -7,7 +7,7 @@ interface LandingpageProps {
 export default function Landingpage({ children }: LandingpageProps) {
   return (
     <div className=" landingly h-screen w-screen text-center placecenter">
-      <h1 className="text-7xl text-wrap h-screen sm:py-36 w-screen text-center sm:text-red-700  md:text-amber-600 md:text-8xl  lg:text-purple-900 lg:hero lg:text-9xl lg:py-0 ">
+      <h1 className="text-6xl text-wrap h-screen sm:py-36 w-screen text-center md:text-amber-600 md:text-8xl  lg:text-purple-900 lg:hero lg:text-9xl lg:py-0 ">
         {children}
       </h1>
     </div>
