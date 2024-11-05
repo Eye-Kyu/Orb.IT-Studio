c2a73468c6f4b3e28edc6d2cafc1a3f55dc181b7
unknown <wankenobi29@gmail.com>
2024-11-05 16:25:38 +0300

    Contact screen mobile-size

--- components/ui/intouch.tsx
+++ components/ui/intouch.tsx
@@ -2,15 +2,17 @@ export default function Intouch() {
   return (
     <div className="w-screen h-screen">
       <div className="intouch flex-col items-center justify-center text-center lg:mx-40 my-40">
-        <div className="lg:text-8xl lg:text-nowrap text-6xl text-wrap">
+        <div className="lg:text-8xl lg:text-nowrap text-5xl text-wrap">
           TALK WITH US
         </div>
-        <p className="mb-20">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-          exercitationem expedita doloribus voluptatibus atque pariatur officiis
-          voluptatem a optio ea, nihil vel similique nam quas veritatis eum
-          deserunt qui. Debitis.
-        </p>
+        <div className="lg:w-screen w-11/12">
+          <p className="mb-20">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            exercitationem expedita doloribus voluptatibus atque pariatur
+            officiis voluptatem a optio ea, nihil vel similique nam quas
+            veritatis eum deserunt qui. Debitis.
+          </p>
+        </div>
 
         <p>Drop us a line at</p>
         <div className="lg:text-7xl text-5xl">hello@Yogi.studio</div>
